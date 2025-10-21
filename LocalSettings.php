f1d4916fd7a9fbd360fc155c249568c3dd0abefe
KimmyWingz <127132791+fishpeopleapps@users.noreply.github.com>
2025-10-21 08:35:38 -0600

    add missing libraries

--- LocalSettings.php
+++ LocalSettings.php
@@ -125,7 +125,7 @@ $wgDiff3 = "/usr/bin/diff3";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector-2022";
+$wgDefaultSkin = "Citizen";
 
 # Enabled skins.
 # The following skins were automatically enabled:
